88825a3f5b91f025b82a997bd0a4c5604909bef0
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2025-12-24 19:04:01 +0530

    Add files via upload

---
+++ Servlet & JSP/java/com/q/Servlet1.java
@@ -0,0 +1,53 @@
+package com.q;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String address = request.getParameter("address");
+		
+		System.out.println("VALIDATE NAME & ADDRESS"+name+"||"+address);
+		
+		response.getWriter().print("VALIDATION SUCCESSFULL<br>");
+
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/Servlet2");
+		//rd.forward(request, response);
+		rd.include(request, response);
+		
+
+	}
+
+}
